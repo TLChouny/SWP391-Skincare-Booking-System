30d38bf28c45987a6ce2b18bc885de11c0a2e636
DatVX02 <143780039+DatVX02@users.noreply.github.com>
2025-01-10 11:33:43 +0700

    fix tailwind css

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,23 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        float: "floatUpDown 3s ease-in-out infinite",
-      },
-      keyframes: {
-        floatUpDown: {
-          "0%, 100%": {
-            transform: "translateY(0)",
-          },
-          "50%": {
-            transform: "translateY(-20px)",
-          },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
