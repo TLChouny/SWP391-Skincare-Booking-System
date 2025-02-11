b44b643070ecd41f08041d8c5256c0dcdea3cfb3
Nguyễn Minh Thành <141386951+ScatterZero@users.noreply.github.com>
2025-02-11 23:55:30 +0700

    Create N Tier Layer

---
+++ BE/LuluSPA/LuluSPA/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using LuluSPA.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuluSPA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        public ServiceController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllServices()
+        {
+            var allServices = dbContext.Services.ToList();
+            return Ok(allServices);
+        }
+    }
+}
