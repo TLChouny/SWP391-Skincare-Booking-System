6e756870ac542466a7ae0a0c0afc72c3d859abac
Nguyễn Minh Thành <141386951+ScatterZero@users.noreply.github.com>
2025-03-19 23:50:13 +0700

    Update CheckoutModal.tsx

--- FE/src/components/Cart/CheckoutModal.tsx
+++ FE/src/components/Cart/CheckoutModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { toast } from "react-toastify";
 import { Booking } from "../../types/booking";
+import { JSX } from "react/jsx-runtime";
 
 interface CheckoutModalProps {
   showModal: boolean;
