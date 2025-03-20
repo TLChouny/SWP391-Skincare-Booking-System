39efd04f6cd70c06d1b6ae136482de6cb6a8a0db
DatVX02 <vxdat13@gmail.com>
2025-03-20 11:20:41 +0700

    fix toast setting

--- FE/src/pages/Home/Settingpage.tsx
+++ FE/src/pages/Home/Settingpage.tsx
@@ -6,6 +6,7 @@ import { useAuth } from "../../context/AuthContext";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "../../layout/Layout";
+
 const API_BASE_URL = "http://localhost:5000";
 
 const SettingPage = () => {
@@ -27,7 +28,7 @@ const SettingPage = () => {
 
   const fetchUserData = async () => {
     if (!token) {
-      toast.error("Authentication required");
+      toast.error("Authentication required", { autoClose: 3000 });
       return;
     }
 
@@ -44,15 +45,15 @@ const SettingPage = () => {
           : `${API_BASE_URL}/default-avatar.png`,
       });
     } catch {
-      toast.error("Failed to fetch user information!");
+      toast.error("Failed to fetch user information!", { autoClose: 3000 });
     } finally {
       setLoading(false);
     }
   };
 
-  const handleFileChange = async (file: File) => {
+  const handleFileChange = async (file) => {
     if (file.size > 10 * 1024 * 1024) {
-      toast.error("File size exceeds 10MB! Please select an image under 10MB.");
+      toast.error("File size exceeds 10MB! Please select an image under 10MB.", );
       return false;
     }
 
@@ -73,11 +74,10 @@ const SettingPage = () => {
 
       toast.success("Account information updated successfully!");
 
+     
       setUser((prevUser) => ({
         ...prevUser,
-        avatar: `${API_BASE_URL}${
-          response.data.user.avatar
-        }?t=${new Date().getTime()}`,
+        avatar: `${API_BASE_URL}${response.data.user.avatar}?t=${new Date().getTime()}`,
       }));
       fetchUserData();
     } catch {
@@ -87,7 +87,7 @@ const SettingPage = () => {
     return false;
   };
 
-  const handleUpdateUser = async (file?: File) => {
+  const handleUpdateUser = async () => {
     if (!token) {
       toast.error("Authentication required");
       return;
@@ -97,29 +97,15 @@ const SettingPage = () => {
     formData.append("username", user.username);
     formData.append("email", user.email);
 
-    if (file) {
-      formData.append("avatar", file);
-    }
-
     try {
-      const response = await axios.put(
-        `${API_BASE_URL}/api/auth/update-profile`,
-        formData,
-        {
-          headers: {
-            "x-auth-token": token,
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      await axios.put(`${API_BASE_URL}/api/auth/update-profile`, formData, {
+        headers: {
+          "x-auth-token": token,
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
       toast.success("Account information updated successfully!");
-      setUser((prevUser) => ({
-        ...prevUser,
-        avatar: `${API_BASE_URL}${
-          response.data.user.avatar
-        }?t=${new Date().getTime()}`,
-      }));
       fetchUserData();
     } catch {
       toast.error("Failed to update account information!");
@@ -152,14 +138,14 @@ const SettingPage = () => {
       setOldPassword("");
       setNewPassword("");
     } catch {
-      toast.error("Failed to change password!");
+      toast.error("Failed to change password!", { autoClose: 3000 });
     }
   };
 
   const handleLogout = () => {
     localStorage.removeItem("authToken");
     localStorage.removeItem("user");
-    toast.success("Đăng xuất thành công!");
+    toast.success("Logout successfully!", { autoClose: 3000 });
     window.location.href = "/login";
   };
 
@@ -168,7 +154,6 @@ const SettingPage = () => {
   return (
     <Layout>
       <div className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-lg mt-10">
-        <ToastContainer />
         <h2 className="text-3xl font-semibold text-center text-gray-800 mb-6">
           Account Settings
         </h2>
@@ -186,76 +171,35 @@ const SettingPage = () => {
           beforeUpload={handleFileChange}
           className="mb-4"
         >
-          <Button
-            icon={<UploadOutlined />}
-            className="bg-blue-500 text-white hover:bg-blue-600"
-          >
-            {" "}
+          <Button icon={<UploadOutlined />} className="bg-blue-500 text-white hover:bg-blue-600">
             Upload Avatar
           </Button>
         </Upload>
 
         <div className="space-y-4">
-          <label className="block text-sm font-medium text-gray-700 mb-1">
-            Username
-          </label>
-          <Input
-            value={user.username}
-            onChange={(e) => setUser({ ...user, username: e.target.value })}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
+          <label className="block text-sm font-medium text-gray-700 mb-1">Username</label>
+          <Input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} />
 
-          <label className="block text-sm font-medium text-gray-700 mb-1">
-            Email
-          </label>
-          <Input
-            value={user.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
+          <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
+          <Input value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
 
-          <Button
-            type="primary"
-            onClick={() => handleUpdateUser()}
-            className="w-full bg-blue-500 text-white hover:bg-blue-600 mt-4"
-          >
+          <Button type="primary" onClick={handleUpdateUser} className="w-full bg-blue-500 text-white hover:bg-blue-600 mt-4">
             Update Information
           </Button>
         </div>
 
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          Old Password
-        </label>
-        <Input.Password
-          value={oldPassword}
-          onChange={(e) => setOldPassword(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+        <label className="block text-sm font-medium text-gray-700 mb-1">Old Password</label>
+        <Input.Password value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} />
 
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          New Password
-        </label>
-        <Input.Password
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+        <label className="block text-sm font-medium text-gray-700 mb-1">New Password</label>
+        <Input.Password value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
 
-        <Button
-          type="default"
-          onClick={handleChangePassword}
-          className="w-full bg-gray-200 text-gray-800 hover:bg-gray-300 mt-4"
-        >
+        <Button type="default" onClick={handleChangePassword} className="w-full bg-gray-200 text-gray-800 hover:bg-gray-300 mt-4">
           Change Password
         </Button>
+
         <div className="text-center mt-8">
-          <Button
-            type="primary"
-            danger
-            onClick={handleLogout}
-            icon={<LogoutOutlined />}
-            className="bg-red-500 text-white hover:bg-red-600"
-          >
+          <Button type="primary" danger onClick={handleLogout} icon={<LogoutOutlined />} className="bg-red-500 text-white hover:bg-red-600">
             Logout
           </Button>
         </div>
