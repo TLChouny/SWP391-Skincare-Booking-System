99fac5b5da93df0e07019f293720c559fa61d60c
hai11062003 <truonggiahai007@gmail.com>
2025-03-20 13:18:57 +0700

    rating

--- FE/src/utils/utils.ts
+++ FE/src/utils/utils.ts
@@ -6,7 +6,7 @@ export const calculateTotalUsers = (users: any[]) => {
   
   // Tính tổng số đơn hàng đã hoàn thành
   export const calculateTotalBookings = (carts: any[]) => {
-    return carts.filter((cart) => cart.status === "completed").length;
+    return carts.filter((cart) => cart.status === "reviewed" || "checked-out").length;
   };
   
 
