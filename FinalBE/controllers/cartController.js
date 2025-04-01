7fb4285af5c82bbb209b018f28d5d5d247c2cf16
DatVX02 <vxdat13@gmail.com>
2025-04-01 11:57:42 +0700

    fix

--- FinalBE/controllers/cartController.js
+++ FinalBE/controllers/cartController.js
@@ -83,7 +83,7 @@ exports.createCart = async (req, res) => {
       const existingBooking = await Cart.findOne({
         bookingDate,
         Skincare_staff,
-        status: { $in: ["pending", "checked-in"] },
+        status: { $in: ["pending", "checked-in", "completed"] },
         $or: [{ startTime: { $lt: endTime }, endTime: { $gt: startTime } }],
       });
 
